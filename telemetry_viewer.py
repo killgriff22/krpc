5d04fb5b3bb2cfc89b70b94645a5e44e956e03ab
killgriff22 <kilgrif22033@gmail.com>
2026-04-12 19:45:43 -0400

    prepare to write flight reporter

--- telemetry_viewer.py
+++ telemetry_viewer.py
@@ -38,6 +38,12 @@ while True:
     print_i += 1
     print_at(1, print_i, f"Z: {velocity[2]}")
     print_i += 1
+
+    print_i = 30
+    print_at(1, print_i, f"Flight Log:")
+    print_i += 1
+    for i, log in enumerate(reports):
+        print_at(0,print_i+i, log['message'])
     print_at_mult(200, 20, """
          /\\
         /  \\

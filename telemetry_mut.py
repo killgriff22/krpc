3e8ed7cd40519675429fc8db511afb653cbf27a9
killgriff22 <kilgrif22033@gmail.com>
2026-04-13 12:32:46 -0400

    filling m9inds with ideas

--- telemetry_mut.py
+++ telemetry_mut.py
@@ -2,4 +2,5 @@ rocketimgx = 60
 rocketimgy = 20
 time_to_next_pull = round(10-dt_1)
 time_to_next_report_pull = round(1-dt_2)
-extra_info = f" TTP:{time_to_next_pull} TTR:{time_to_next_report_pull}"
+extra_info = (
+    f" TTP:{time_to_next_pull} TTR:{time_to_next_report_pull}"+(" "*40))[:40]

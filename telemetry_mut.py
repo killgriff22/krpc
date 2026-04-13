61755a55f0ac03dc6a2661f1a6d7e835d22f4212
killgriff22 <kilgrif22033@gmail.com>
2026-04-13 12:29:03 -0400

    fuck part 2

--- telemetry_mut.py
+++ telemetry_mut.py
@@ -1,5 +1,5 @@
 rocketimgx = 60
 rocketimgy = 20
-time_to_next_pull = round(10-dt)
-time_to_next_report_pull = round(1-(dt % 1))
+time_to_next_pull = round(10-dt_1)
+time_to_next_report_pull = round(1-dt_2)
 extra_info = f" TTP:{time_to_next_pull} TTR:{time_to_next_report_pull}"

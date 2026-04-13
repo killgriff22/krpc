f3904dd8d0122e333f3767e5104e4ec210c099db
killgriff22 <kilgrif22033@gmail.com>
2026-04-13 12:09:57 -0400

    push everything

--- GuidedAscent.py
+++ GuidedAscent.py
@@ -2,11 +2,12 @@ import krpc
 import time
 from utils import *
 FLIGHT_PROFILE = {
-    f"Target Alt": 40000,
-    f"Descent Alt": 2000,
-    f"Orbit Burn": False,
-    f"Return Descent": True,
-    f"Carrying Payload": False,
+    "Target Apo": 4000,
+    "Descent Alt": 2000,
+    "Orbit Burn": False,
+    "Return Descent": True,
+    "Carrying Payload": False,
+    "Payload Drop Height": 50000,
 }
 conn = krpc.connect(name='Ascent Guidance', address=KRPC_SERVER)
 vessel = conn.space_center.active_vessel
@@ -19,17 +20,20 @@ input("Flight Plan sent! await ready to launch!")
 ut = conn.add_stream(getattr, conn.space_center, 'ut')
 altitude = conn.add_stream(getattr, vessel.flight(), 'mean_altitude')
 apoapsis = conn.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
+ref_frame = conn.space_center.ReferenceFrame.create_hybrid(
+    position=vessel.orbit.body.reference_frame,
+    rotation=vessel.surface_reference_frame)
 ap = vessel.auto_pilot
 ap.reference_frame = vessel.surface_velocity_reference_frame
 vessel.auto_pilot.engage()
 vessel.control.throttle = 1
 time.sleep(1)
 report_message('Launch!')
-# vessel.control.activate_next_stage()
+vessel.control.activate_next_stage()
 while altitude() < 1000:
     pass
 control.gear = False
-while apoapsis() < FLIGHT_PROFILE['Descent Alt']:
+while apoapsis() < FLIGHT_PROFILE['Target Apo']:
     pass
 
 
@@ -37,12 +41,47 @@ report_message('Throttle Back')
 vessel.control.throttle = 0
 time.sleep(2)
 
+if FLIGHT_PROFILE['Carrying Payload']:
+    report_message('Await Drop Height')
+    while altitude() < FLIGHT_PROFILE['Payload Drop Height']:
+        pass
+else:
+    report_message('Await Apoapsis')
+    delta_altapo = apoapsis() - altitude()
+    t = time.time()
+    while delta_altapo > 100:
+        velocity = vessel.flight(ref_frame).velocity
+        _t = time.time()
+        delta_altapo = apoapsis() - altitude()
+        if _t-t >= .5:
+            report_message(f"Time to Apoapsis: {delta_altapo/velocity[0]}")
+            report_message(f"Delta to Apoapsis: {delta_altapo}")
+            t = _t
+        pass
+report_message('Faring separation')
+vessel.control.activate_next_stage()
+
+
+report_message('Prepare for Reentry')
+
+if altitude() > 10000:
+    if not control.get_action_group(4):
+        control.toggle_action_group(4)
+
+
 report_message('Secondary parachute deployment')
 vessel.control.toggle_action_group(2)
 
-time.sleep(10)
-report_message('Faring separation')
-vessel.control.activate_next_stage()
+
+input("await user to circularize payload!\nPress enter when ready!")
+
+while altitude() > 10000:
+    pass
+
+report_message('Fold Ablatives')
+control.toggle_action_group(4)
+
+
 report_message('Retrograde')
 # Point the vessel in the retrograde direction
 ap.target_direction = (0, -1, 0)
@@ -50,9 +89,7 @@ ap.target_direction = (0, -1, 0)
 report_message('Brakes')
 control.brakes = True
 
-ref_frame = conn.space_center.ReferenceFrame.create_hybrid(
-    position=vessel.orbit.body.reference_frame,
-    rotation=vessel.surface_reference_frame)
+
 parachute_flag_b = False
 parachute_flag_a = False
 report_message("begin control loop")
@@ -60,6 +97,8 @@ while FLIGHT_PROFILE["Return Descent"]:
     velocity = vessel.flight(ref_frame).velocity
     alt = altitude()
     if alt < FLIGHT_PROFILE['Descent Alt']:
+        if not control.gear:
+            control.gear = True
         if not parachute_flag_b:
             report_message('Pop Primary Parachute')
             vessel.control.toggle_action_group(1)
---
+++ ListVessels.py
@@ -0,0 +1,85 @@
+import krpc
+import time
+import math
+from utils import *
+
+# exit()
+
+conn = krpc.connect(name='Telemetry', address=KRPC_SERVER)
+# input("Press Enter to continue...")
+spc = conn.space_center
+clear()
+t = time.time()
+ns = time.time_ns()
+_ns = time.time_ns()
+lastcontent = ""
+x = 100
+y = 10
+ActiveVessel = None
+pagecontent = """
+  00000000000000001111111111111111222222222222222333333333333333444444444444444
+  01234567879ABCEF0123456789ABCDEF0123456789ABCEF0123456789ABCEF0123456789ABCEF
+00┌───────────────────────────────────────────────────────────────┐            
+01│                                                 /\\           │            
+02│                                                /  \\          │            
+03│                    \\|   \\                     /    \\         │            
+04│                   \\\\\\ \\ \\                    /______\\        │            
+05│                     \\\\  \\  \\                |        |        │            
+06│                     \\ \\   \\-                |        |        │            
+07│                     \\\\-\\   \\\\\\              |        |        │            
+08│                      ┌─┐   ┌─┐              |        |        │            
+09│                      │ │   │ │              |        |        │            
+0A│                      │ │   │ │              |        |        │            
+0B│                      │ │   │ │             /|   ||   |\\      │            
+0C│                      │ │   │ │            / |   ||   | \\     │            
+0D│                      │ │   │ │           /  |   ||   |  \\    │            
+0E│  ┌───────────────────│ │───│ │          /___|   ||   |___\\   │            
+0F│  │                   │ │   │ │              |        |        │            
+10│  │        ┌────┐     │ │   │ │               \\      /         │            
+11│  │        │    │     │ │   │ │                ||  ||          │            
+12│  │        └────┘     │ │   │ │           ┌──────────────┐     │            
+13│  │  ┌───┐            │ │   │ │          /    /          │     │            
+14│  │  │   │            │ │   │ │     ┌───/    /           │     │            
+15│  │  │   │            └─┘   └─┘     │  /    /            |     │            
+16│  │  │   │                    │  ┌────/────/─────────────|───┐ │            
+17│  └──└───┘────────────────────┘  └───────────────────────────┘ │            
+18└───────────────────────────────────────────────────────────────┘            
+19                                     └────────┘                              
+1A                                                                             
+1B                                                                             
+1C                                                                             
+1D                                                                             
+1E                                                                             
+1F                                                                             
+"""
+while True:
+    _t = time.time()
+    dt = _t-t
+    if dt > 10:
+        t = _t
+        clear()
+    print_i = 1
+    print_at(1, print_i, f"frametime {(_ns-ns)/1000000000}")
+    print_i += 1
+    print_at(
+        1, print_i, f"Active Vessel: {""}")
+    print_i += 1
+    print_at(
+        1, print_i, f"Vessels:")
+    print_i += 1
+    for ls in spc.launch_sites:
+        print_at(
+            1, print_i, f"{ls.name}: {ls.body.name}")
+        print_i += 1
+    for v in spc.vessels:
+        print_at(
+            1, print_i, f"{v.name}: {v.recoverable} {v.crew}")
+        print_i += 1
+    content = open("muts.py", "r").read()
+    if not content == lastcontent:
+        lastcontent = content
+        clear()
+    exec(compile(f"{content}", "bleh.py", "exec"))
+    print_at_mult(x, y, pagecontent)
+    ns = _ns
+    _ns = time.time_ns()

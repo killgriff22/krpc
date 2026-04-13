a29445f4cf4dcac35c5145c996c66e5f45ec1a8e
killgriff22 <kilgrif22033@gmail.com>
2026-04-13 12:50:27 -0400

    vlehhs

--- telemetry_viewer.py
+++ telemetry_viewer.py
@@ -58,31 +58,33 @@ while True:
         1, print_i, f"frametime {round((_ns-ns)/1000000000)} {extra_info}")
     print_i += 1
     print_at(
-        1, print_i, f"Vessel: {vessel.name} SAS: {control.sas} RCS: {control.rcs} Throttle: {round(control.throttle*100)/100}            ")
+        1, print_i, f"Vessel: {vessel.name} SAS: {control.sas} RCS: {control.rcs} Throttle: {round(control.throttle*100)/100}")
     print_i += 1
     print_at(
-        1, print_i, f"Brakes: {control.brakes} Gear: {control.gear} Antennas: {control.antennas} Cargo Bays: {control.cargo_bays} Lights: {control.lights}            ")
+        1, print_i, f"Brakes: {control.brakes} Gear: {control.gear} Antennas: {control.antennas} Cargo Bays: {control.cargo_bays} Lights: {control.lights}")
     print_i += 1
     print_at(
-        1, print_i, f"Radiators: {control.radiators} Panels: {control.solar_panels} Antennas: {control.antennas} Stage: {control.current_stage} Abort: {control.abort}            ")
+        1, print_i, f"Radiators: {control.radiators} Panels: {control.solar_panels} Antennas: {control.antennas} Stage: {control.current_stage} Abort: {control.abort}")
     print_i += 1
-    print_at(1, print_i, f"Stage: {control.current_stage}             ")
+    print_at(1, print_i, f"Stage: {control.current_stage}")
     print_i += 1
-    print_at(1, print_i, f"Altitude: {alt} Apoapsis: {apo}            ")
+    print_at(1, print_i, f"Altitude: {alt} Apoapsis: {apo}")
     print_i += 1
-    print_at(1, print_i, f"Velocity, RPH:            ")
-    print_i += 1
-    print_at(1, print_i, f"X: {velocity[0]}     R: {control.roll}            ")
+    print_at(1, print_i, f"Velocity, RPH:")
     print_i += 1
     print_at(
-        1, print_i, f"Y: {velocity[1]}     P: {control.pitch}            ")
-    print_i += 1
-    print_at(1, print_i, f"Z: {velocity[2]}     H: {control.yaw}            ")
+        1, print_i, f"X: {round(velocity[0])}     R: {round(control.roll)}")
     print_i += 1
     print_at(
-        1, print_i, f"Time to Surface: {alt/(abs(velocity[0])+1)}            ")
+        1, print_i, f"Y: {round(velocity[1])}     P: {round(control.pitch)}")
     print_i += 1
-    print_at(1, print_i, f"Flight Profile:            ")
+    print_at(
+        1, print_i, f"Z: {round(velocity[2])}     H: {round(control.yaw)}")
+    print_i += 1
+    print_at(
+        1, print_i, f"Time to Surface: {alt/(abs(velocity[0])+1)}")
+    print_i += 1
+    print_at(1, print_i, f"Flight Profile:")
     print_i += 1
     for i, k in enumerate(list(flight_profile.keys())):
         print_at(1, i+print_i, f"{k}: {flight_profile[k]}")

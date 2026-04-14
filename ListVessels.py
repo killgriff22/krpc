b14fd0f6fe41c3746fb64baf6548c959e6b7d9f4
killgriff22 <kilgrif22033@gmail.com>
2026-04-13 20:21:14 -0400

    invert

--- ListVessels.py
+++ ListVessels.py
@@ -21,7 +21,7 @@ displays = cluster()
 display = Screen((w-SafeZone[0]*2, (h-SafeZone[1]*2)), SafeZone)
 displays.screens.append(display)
 while True:
-    display.fill(error(" "))
+    display.fill(" ")
     time = datetime.datetime.now()
     H = 12 if int(time.strftime("%H")) % 12 == 0 else int(
         time.strftime("%H")) % 12
@@ -29,12 +29,12 @@ while True:
         f"{H}/%M/%S"))
     ampm = f.renderText(time.strftime("%p"))
     Date = f.renderText(time.strftime("%d/%m/%Y"))
-    display.blit(Hours, (display.size[0]//2-len(Hours.split("\n")[0])//2, display.size[1]//2-len(Hours.split("\n"))//2), front_modifier=Fore.BLACK +
+    display.blit(Hours, (display.size[0]//2-len(Hours.split("\n")[0])//2, display.size[1]//2-len(Hours.split("\n"))//2), front_modifier=Fore.RED +
                  Back.RED, back_modifier=RESET)
     display.blit(ampm, (display.size[0]//2+len(Hours.split("\n")[0])//2+1, display.size[1]//2-len(ampm.split("\n"))//2),
-                 front_modifier=Fore.BLACK+Back.RED, back_modifier=RESET)
+                 front_modifier=Fore.RED+Back.RED, back_modifier=RESET)
     display.blit(Date, (display.size[0]//2-len(Date.split("\n")[0])//2, display.size[1]//2+3),
-                 front_modifier=Fore.BLACK+Back.RED, back_modifier=RESET)
+                 front_modifier=Fore.RED+Back.RED, back_modifier=RESET)
     displays.draw_all()
 
 
